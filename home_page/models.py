000b30fc9bde94ab5b5ffff98a8aff76ddc33468
aalarifi2016 <aalarifi2016@my.fit.edu>
2021-02-24 16:46:57 -0500

    t

--- home_page/models.py
+++ home_page/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+import yfinance as yf
+
+def get_price(ticker):
+    stock = yf.Ticker("AAPL")
+
+    # get stock price
+    return stock.info['previousClose']
--- home_page/views.py
+++ home_page/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import get_price
 # Create your views here.
+
+
 def home_view(request):
+
+    if request.method == "POST":
+        price = get_price(request.POST.get('ticker'))
+        
+        return render(request, 'home.html', {"price": price})
+
     return render(request, 'home.html', {})
 
 
