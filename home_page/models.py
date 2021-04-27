07e4610cc5762bca0b24a71bcaf3155eafabcf05
aalarifi2016 <aalarifi2016@my.fit.edu>
2021-04-27 00:44:21 -0400

    add TODOs

--- home_page/models.py
+++ home_page/models.py
@@ -2,18 +2,19 @@ from django.db import models
 
 
 # Create your models here.
-import yfinance as yf
 from yahoo_fin import stock_info as si
 from newsapi import NewsApiClient
 
 
 def get_price(ticker):
+    #TODO: write a scrapping code to get the price from yahoo finance
     stock = si.get_live_price(ticker)
 
     # get stock price
     return round(stock, 2)
 
 def get_news(ticker):
+    #TODO: replace this api with scrapping tool to get the recent news from yahoo or reuters 
     # Init
     newsapi = NewsApiClient(api_key='136424f10edd4de7b7f35adb9edaf0de')
 
--- home_page/tests.py
+++ home_page/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from requests import models
+
 
 # Create your tests here.
+#TODO: create a test for "get_price"
+def test_get_price():
+    
+    pass
+
+#TODO: create a test for "get_news"
+def test_get_news():
+    pass
