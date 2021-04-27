fee93cc0a074b6c7df5bd44cd0514345b7a285ab
aalarifi2016 <aalarifi2016@my.fit.edu>
2021-04-27 00:43:46 -0400

    add some comments

--- home_page/views.py
+++ home_page/views.py
@@ -6,11 +6,13 @@ from .models import get_price, get_news
 
 def home_view(request):
 
+    # read the input from the user
     if request.method == "POST":
         price = get_price(request.POST.get('ticker'))
         news = get_news(request.POST.get('ticker'))
 
         # format the news
+        #TODO: this line needs to be replaced after fixing the get_news function
         news = [a['content'] for a in news['articles']]
         
         return render(request, 'home.html', {"price": price, "news": news})
