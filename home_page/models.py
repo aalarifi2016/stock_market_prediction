ce6904ebdc216227b4e71b54ca857c7e69daa23d
aalarifi2016 <aalarifi2016@my.fit.edu>
2021-04-28 06:12:00 -0400

    refactor the code

--- home_page/models.py
+++ home_page/models.py
@@ -1,34 +1,155 @@
 from django.db import models
 
-
 # Create your models here.
-from yahoo_fin import stock_info as si
-from newsapi import NewsApiClient
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
 
 
-def get_price(ticker):
-    #TODO: write a scrapping code to get the price from yahoo finance
-    stock = si.get_live_price(ticker)
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
 
-    # get stock price
-    return round(stock, 2)
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
 
-def get_news(ticker):
-    #TODO: replace this api with scrapping tool to get the recent news from yahoo or reuters 
-    # Init
-    newsapi = NewsApiClient(api_key='136424f10edd4de7b7f35adb9edaf0de')
 
-    # /v2/everything
-    all_articles = newsapi.get_everything(q=ticker,
-                                        sources='bbc-news,the-verge',
-                                        domains='bbc.co.uk,techcrunch.com',
-                                        from_param='2021-04-09',
-                                        to='2021-04-23',
-                                        language='en',
-                                        sort_by='relevancy',
-                                        page=2)
+class AuthPermission(models.Model):
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
 
-    # /v2/sources
-    sources = newsapi.get_sources()
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
 
-    return all_articles
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+    first_name = models.CharField(max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    action_flag = models.PositiveSmallIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class HistoricalData(models.Model):
+    ticker = models.CharField(max_length=4)
+    date = models.IntegerField(db_column='Date')  # Field name made lowercase.
+    adj_close = models.DecimalField(db_column='Adj Close', max_digits=10, decimal_places=5, blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. max_digits and decimal_places have been guessed, as this database handles decimal fields as float
+
+    class Meta:
+        managed = False
+        db_table = 'historical_data'
+
+
+class ReutersNews(models.Model):
+    ticker = models.TextField(blank=True, null=True)
+    date = models.TextField(blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    sentiment = models.IntegerField(blank=True, null=True)
+    polarity = models.IntegerField(blank=True, null=True)
+    subjectivity = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'reuters_news'
+
+
+class SqliteStat1(models.Model):
+    tbl = models.TextField(blank=True, null=True)  # This field type is a guess.
+    idx = models.TextField(blank=True, null=True)  # This field type is a guess.
+    stat = models.TextField(blank=True, null=True)  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'sqlite_stat1'
+
+
+class StockNames(models.Model):
+    ticker = models.CharField(max_length=8)
+    name = models.CharField(max_length=64)
+    sector = models.CharField(max_length=32)
+
+    class Meta:
+        managed = False
+        db_table = 'stock_names'
--- home_page/views.py
+++ home_page/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import get_price, get_news
 # Create your views here.
+
+from yahoo_fin import stock_info as si
+from newsapi import NewsApiClient
+
+def get_price(ticker):
+    #TODO: write a scrapping code to get the price from yahoo finance
+    stock = si.get_live_price(ticker)
+
+    # get stock price
+    return round(stock, 2)
+
+def get_news(ticker):
+    #TODO: replace this api with scrapping tool to get the recent news from yahoo or reuters 
+    # Init
+    newsapi = NewsApiClient(api_key='136424f10edd4de7b7f35adb9edaf0de')
+
+    # /v2/everything
+    all_articles = newsapi.get_everything(q=ticker,
+                                        sources='bbc-news,the-verge',
+                                        domains='bbc.co.uk,techcrunch.com',
+                                        from_param='2021-04-09',
+                                        to='2021-04-23',
+                                        language='en',
+                                        sort_by='relevancy',
+                                        page=2)
+
+    # /v2/sources
+    sources = newsapi.get_sources()
+
+    return all_articles
 
 
 def home_view(request):
