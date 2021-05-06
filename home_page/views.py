2f6f47c8798b4a9ac621df6158d1f9b17c67d434
aalarifi2016 <aalarifi2016@my.fit.edu>
2021-05-06 05:19:40 -0400

    add the recent news to the webpage

--- home_page/views.py
+++ home_page/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from libs.get_data import Ticker
 # Create your views here.
 
 from yahoo_fin import stock_info as si
 # from newsapi import NewsApiClient
 
 def get_price(ticker):
+
     #TODO: write a scrapping code to get the price from yahoo finance
     stock = si.get_live_price(ticker)
 
@@ -14,19 +16,8 @@ def get_price(ticker):
 
 def get_news(ticker):
     #TODO: replace this api with scrapping tool to get the recent news from yahoo or reuters 
-    # Init
-    # newsapi = NewsApiClient(api_key='136424f10edd4de7b7f35adb9edaf0de')
-
-    # /v2/everything
-    # all_articles = newsapi.get_everything(q=ticker,
-    #                                     sources='bbc-news,the-verge',
-    #                                     domains='bbc.co.uk,techcrunch.com',
-    #                                     from_param='2021-04-09',
-    #                                     to='2021-04-23',
-    #                                     language='en',
-    #                                     sort_by='relevancy',
-    #                                     page=2)
-
+    data = Ticker(ticker)
+    
     
 
     return []
@@ -36,14 +27,15 @@ def home_view(request):
 
     # read the input from the user
     if request.method == "POST":
+        data = Ticker(request.POST.get('ticker'))
         price = get_price(request.POST.get('ticker'))
-        # news = get_news(request.POST.get('ticker'))
+        news = data.get_recent_news(source='reuters')
 
         # format the news
         #TODO: this line needs to be replaced after fixing the get_news function
         # news = [a['content'] for a in news['articles']]
         
-        return render(request, 'home.html', {"price": price, "news": []})
+        return render(request, 'home.html', {"price": price, "news": news})
 
     return render(request, 'home.html', {})
 
