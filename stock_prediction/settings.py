b9b6ec8d54d31f394861e6d9f66b74e4f5a22b8d
thabet alenezi <thabetalenezi@thabets-MacBook-Air.local>
2021-11-30 23:58:47 -0500

    added bootstrap

--- stock_prediction/settings.py
+++ stock_prediction/settings.py
@@ -128,5 +128,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+
+    #bootstrap
+    # os.path.join(BASE_DIR, 'boot'),
+
+
 ]
