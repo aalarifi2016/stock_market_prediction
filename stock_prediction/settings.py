41abbe5fc97f32d796a8db5ae5085de39c009bb2
aalarifi2016 <aalarifi2016@my.fit.edu>
2021-12-10 14:52:18 -0500

    f

--- stock_prediction/settings.py
+++ stock_prediction/settings.py
@@ -174,12 +174,12 @@ LOGGING = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     # bootstrap
-#     # os.path.join(BASE_DIR, 'boot'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # bootstrap
+    # os.path.join(BASE_DIR, 'boot'),
+]
 
 django_heroku.settings(locals())
