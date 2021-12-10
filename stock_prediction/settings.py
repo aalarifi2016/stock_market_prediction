1990f9ba900d5eefba6ea1247317bcca99ab2aa2
aalarifi2016 <aalarifi2016@my.fit.edu>
2021-12-10 14:44:08 -0500

    f

--- stock_prediction/settings.py
+++ stock_prediction/settings.py
@@ -33,12 +33,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    ".herokuapp.com",
-    "https://stockmarketpredictionfit.herokuapp.com/",
-]
-
+# ALLOWED_HOSTS = [
+#     "localhost",
+#     ".herokuapp.com",
+#     "https://stockmarketpredictionfit.herokuapp.com/",
+# ]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
